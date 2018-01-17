ed3b01326afcffba41f13d74a58eaa7a95425a37
LilleJohs <33736881+LilleJohs@users.noreply.github.com>
2018-01-16 20:35:12 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head ><link rel="shortcut icon" type="image/x-icon" href="raiblocks.png" />
+	<title class="Title">Spend RaiBlocks</title>
+	<link rel="stylesheet" type="text/css" href="index2.css">
+</head>
+
+<body>
+	<div class= "topBar">
+	  <span class= "button"><a class="buttonLink" href="map.html">Map</a></span>
+	  <h1 class = "title">Spend RaiBlocks</h1>
+	</div>
+
+	<div class = "infoBox">
+	
+	<p> RaiBlocks is a global currency and should therefore be used as one. You can pay with Rai at any of the websites below.</p>
+	
+	<img class ="infoImage" src="logo.png" alt="Raiblocks" width="80" height="80">
+	
+	<p> Don't know what RaiBlocks is? Visit the <a class = "link" href="https://www.raiblocks.net"> Official Raiblocks Website</a> and the <a class="link" href="https://www.reddit.com/r/RaiBlocks"> RaiBlocks Subreddit</a> to learn more about the currency!</p>
+	<p> Want to accept RaiBlocks on your own website but don't know how? Check out <a class = "link" href=https://raipay.io/>RaiPay</a>, <a class = "link" href=https://www.getxrb.com/>GetXRB</a> and <a class = "link" href=https://brainblocks.io/>brainblocks</a>!</p>
+	</div>
+	
+	<table>
+	<tr>
+		<th>Website</th>
+		<th>Category</th>
+	</tr>
+	<?php include "database.php";?>
+	</table>
+	
+	<div class = "email">
+		<p> Do you know a website that accepts RaiBlocks but isn't listed here? Fill out this <a href= https://goo.gl/forms/0QoUhojEWuetMzbn1>listing form</a>.</p>
+		
+		<p> Want to get in touch? Send an e-mail to <a href= mailto:lillejohs@spendraiblocks.com>lillejohs@spendraiblocks.com</a>.</p>
+	</div>
+</body>
+
+</html>
